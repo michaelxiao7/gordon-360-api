9ece9d7284a0037bb25d1b2b1272094ccc940deb
Cameron Abbot <Cameron.Abbot@gordon.edu>
2021-06-22 16:28:47 -0400

    started work on startup file, part one of controller refactoring

--- Gordon360/Startup.cs
+++ Gordon360/Startup.cs
@@ -39,6 +39,20 @@ namespace Gordon360
                     options.UseMemberCasing();
                 });
 
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("https://360.gordon.edu", "https://360train.gordon.edu", "http://localhost:3000");
+                    });
+            });
+
+            services.AddSignalR().AddJsonProtocol(options =>
+            {
+                options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,12 +66,12 @@ namespace Gordon360
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
+            app.UseCors();
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                TokenEndPointPath = new PathStrin
+            })
+
         }
 
         private void ConfigureMvcOptions(MvcOptions mvcOptions)
